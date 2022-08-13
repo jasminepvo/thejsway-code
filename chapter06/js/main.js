e69ba52cc207263e64da75e14509056e5b15b145
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-13 19:55:29 -0400

    Create circle object

--- chapter06/js/main.js
+++ chapter06/js/main.js
@@ -47,6 +47,14 @@ Complete the following program to add the circle object definition. Its radius v
 const r = Number(prompt("Enter the circle radius:"));
 
 // TODO: create the circle object here
+const circle = {
+	circumference() {
+		return 2 * Math.PI * r;
+	},
+	area() {
+		return Math.PI * Math.pow(r, 2);
+	},
+};
 
 console.log(`Its circumference is ${circle.circumference()}`);
 console.log(`Its area is ${circle.area()}`);
