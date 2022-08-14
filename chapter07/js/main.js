e034a993972285b5fd6290ad60cd430f7d1d0389
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-13 20:28:33 -0400

    Create array, for of loop each element, calculate and show sum of its values

--- chapter07/js/main.js
+++ chapter07/js/main.js
@@ -29,9 +29,13 @@ for (const musketeer of musketeers) {
 /*
 Sum of values
 Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
-
-const values = [3, 11, 7, 2, 9, 10];
 */
+const values = [3, 11, 7, 2, 9, 10];
+let sum = 0;
+for (const value of values) {
+	sum += value;
+	console.log(sum);
+}
 
 /*
 Array maximum
