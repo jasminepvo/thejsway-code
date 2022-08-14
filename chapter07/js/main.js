0503e02546a9b8ec20bb18286bf16c16e2a175ef
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-13 20:26:03 -0400

    Create array, show each array element using for loop, forEach() method, for-of loop, push element to end of array, splice 1 element starting at index

---
+++ chapter07/js/main.js
@@ -0,0 +1,46 @@
+/*
+Coding time!
+Create all these programs in a generic fashion: the program output should reflect any update in the array's content.
+
+Musketeers
+Write a program that:
+*/
+//Creates an array named musketeers containing values "Athos", "Porthos" and "Aramis".
+const musketeers = ["Athos", "Porthos", "Aramis"];
+
+//Shows each array element using a for loop.
+for (let i = 0; i < musketeers.length; i++) {
+	console.log(musketeers[i]);
+}
+
+//Adds the "D'Artagnan" value to the array.
+musketeers.push("D'Artagnan");
+
+//Shows each array element using the forEach() method.
+musketeers.forEach((musketeer) => console.log(musketeer));
+
+//Remove poor Aramis.
+musketeers.splice(2, 1); //remove 1 element starting at index 2
+//Shows each array element using a for-of loop.
+for (const musketeer of musketeers) {
+	console.log(musketeer);
+}
+
+/*
+Sum of values
+Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
+
+const values = [3, 11, 7, 2, 9, 10];
+*/
+
+/*
+Array maximum
+Write a program that creates the following array, then calculates and shows the array's maximum value.
+
+const values = [3, 11, 7, 2, 9, 10];
+*/
+
+/*
+List of words
+Write a program that asks the user for a word until the user types "stop". The program then shows each of these words, except "stop".
+*/
