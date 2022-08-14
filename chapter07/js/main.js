30ae512f6310207e83472ec30828b4ba980a4d95
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-13 20:35:17 -0400

    Create array and use Math.max(...array) method to show arrays maximum value

--- chapter07/js/main.js
+++ chapter07/js/main.js
@@ -40,9 +40,9 @@ for (const value of values) {
 /*
 Array maximum
 Write a program that creates the following array, then calculates and shows the array's maximum value.
-
-const values = [3, 11, 7, 2, 9, 10];
 */
+const values1 = [3, 11, 7, 2, 9, 10];
+console.log(Math.max(...values1));
 
 /*
 List of words
